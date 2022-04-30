bbab62d616f7fa5db41e12c94693a5ce38c8c0a2
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2022-04-30 14:00:59 -0500

    slider

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ function seleccionarProducto(pos){
 
 
 function loadProduct() {
-
+showSlides(slideIndex);
    let index = localStorage.getItem('index');
    if(index==null){
      self.location = "home.html";
@@ -62,7 +62,34 @@ function loadProduct() {
 
 
 
+let slideIndex = 1;
 
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 
 
